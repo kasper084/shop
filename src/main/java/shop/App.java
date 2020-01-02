ea25eeabe4952b6083844b8529169074cd0f58f2
kasper084 <kasper084@gmail.com>
2020-01-02 18:35:18 +0200

    dev branch

--- src/main/java/shop/App.java
+++ src/main/java/shop/App.java
@@ -1,13 +1,8 @@
 package shop;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
     }
 }
