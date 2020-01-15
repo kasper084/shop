b6ed0ab924fdffd05d1d0b5ab0b8cceb0bfc8f04
kasper084 <kasper084@gmail.com>
2020-01-15 23:45:52 +0200

    app starts with login menu

--- src/main/java/shop/App.java
+++ src/main/java/shop/App.java
@@ -1,13 +1,10 @@
 package shop;
 
 import shop.menu.LoginMenu;
-import shop.menu.admin.UsersMenu;
 
 public class App {
     public static void main(String[] args) {
-/*        LoginMenu loginMenu = new LoginMenu();
-        loginMenu.show();*/
-        UsersMenu usersMenu = new UsersMenu();
-        usersMenu.show();
+        LoginMenu loginMenu = new LoginMenu();
+        loginMenu.show();
     }
 }
